9b3319d361f5b7cfb10d1c1b96d1605b6ef0d84e
Soul <437665122@qq.com>
2020-06-19 13:18:15 +0800

    随便上传

--- src/router.ts
+++ src/router.ts
@@ -4,249 +4,249 @@ import Router from "vue-router"
 Vue.use(Router)
 
 const route = new Router({
-    mode: "history",
-    routes: [
-        //  前台
-        {
-            path: "/",
-            name: "home",
-            // @ts-ignore
-            component: () => import("./views/home.vue")
-        },
-        {
-            path: "/articles",
-            name: "articles",
-            component: () =>
+  mode: "history",
+  routes: [
+    //  前台
+    {
+      path: "/",
+      name: "home",
+      // @ts-ignore
+      component: () => import("./views/home.vue")
+    },
+    {
+      path: "/articles",
+      name: "articles",
+      component: () =>
                 // @ts-ignore
                 import("./views/articles.vue")
-        },
-        {
-            path: "/timeline",
-            name: "timeline",
-            component: () =>
+    },
+    {
+      path: "/timeline",
+      name: "timeline",
+      component: () =>
                 // @ts-ignore
                 import("./views/timeline.vue")
-        },
-        {
-            path: "/friend",
-            name: "friend",
-            component: () =>
+    },
+    {
+      path: "/friend",
+      name: "friend",
+      component: () =>
                 // @ts-ignore
                 import("./views/friend.vue")
-        },
-        {
-            path: "/articleDetail",
-            name: "articleDetail",
-            component: () =>
+    },
+    {
+      path: "/articleDetail",
+      name: "articleDetail",
+      component: () =>
                 // @ts-ignore
                 import("./views/articleDetail.vue")
-        },
+    },
 
-        // 后台
+    // 后台
+    {
+      path: "/admin",
+      component: (resolve) => require(["./views/admin/admin.vue"], resolve),
+      children: [
         {
-            path: "/admin",
-            component: (resolve) => require(["./views/admin/admin.vue"], resolve),
-            children: [
-                {
-                    path: "",
-                    name: "admin",
-                    component: (resolve) => require(["./views/admin/adminDashboard.vue"], resolve)
-                }
-            ]
-        },
+          path: "",
+          name: "admin",
+          component: (resolve) => require(["./views/admin/adminDashboard.vue"], resolve)
+        }
+      ]
+    },
+    {
+      path: "/admin-article",
+      component: (resolve) => require(["./views/admin/admin.vue"], resolve),
+      children: [
         {
-            path: "/admin-article",
-            component: (resolve) => require(["./views/admin/admin.vue"], resolve),
-            children: [
-                {
-                    path: "",
-                    name: "admin-article",
-                    component: (resolve) => require(["./views/admin/adminArticle.vue"], resolve)
-                },
-            ]
-        },
+          path: "",
+          name: "admin-article",
+          component: (resolve) => require(["./views/admin/adminArticle.vue"], resolve)
+        }
+      ]
+    },
+    {
+      path: "/admin-classify",
+      component: (resolve) => require(["./views/admin/admin.vue"], resolve),
+      children: [
         {
-            path: "/admin-classify",
-            component: (resolve) => require(["./views/admin/admin.vue"], resolve),
-            children: [
-                {
-                    path: "",
-                    name: "admin-classify",
-                    component: (resolve) => require(["./views/admin/adminClassify.vue"], resolve)
-                },
-            ]
-        },
+          path: "",
+          name: "admin-classify",
+          component: (resolve) => require(["./views/admin/adminClassify.vue"], resolve)
+        }
+      ]
+    },
+    {
+      path: "/admin-label",
+      component: (resolve) => require(["./views/admin/admin.vue"], resolve),
+      children: [
         {
-            path: "/admin-label",
-            component: (resolve) => require(["./views/admin/admin.vue"], resolve),
-            children: [
-                {
-                    path: "",
-                    name: "admin-label",
-                    component: (resolve) => require(["./views/admin/adminLabel.vue"], resolve)
-                },
-            ]
-        },
+          path: "",
+          name: "admin-label",
+          component: (resolve) => require(["./views/admin/adminLabel.vue"], resolve)
+        }
+      ]
+    },
+    {
+      path: "/admin-articleLabel",
+      component: (resolve) => require(["./views/admin/admin.vue"], resolve),
+      children: [
         {
-            path: "/admin-articleLabel",
-            component: (resolve) => require(["./views/admin/admin.vue"], resolve),
-            children: [
-                {
-                    path: "",
-                    name: "admin-articleLabel",
-                    component: (resolve) => require(["./views/admin/adminArticleLabel.vue"], resolve)
-                },
-            ]
-        },
+          path: "",
+          name: "admin-articleLabel",
+          component: (resolve) => require(["./views/admin/adminArticleLabel.vue"], resolve)
+        }
+      ]
+    },
+    {
+      path: "/admin-discuss",
+      component: (resolve) => require(["./views/admin/admin.vue"], resolve),
+      children: [
         {
-            path: "/admin-discuss",
-            component: (resolve) => require(["./views/admin/admin.vue"], resolve),
-            children: [
-                {
-                    path: "",
-                    name: "admin-discuss",
-                    component: (resolve) => require(["./views/admin/adminDiscuss.vue"], resolve)
-                },
-            ]
-        },
+          path: "",
+          name: "admin-discuss",
+          component: (resolve) => require(["./views/admin/adminDiscuss.vue"], resolve)
+        }
+      ]
+    },
+    {
+      path: "/admin-file",
+      component: (resolve) => require(["./views/admin/admin.vue"], resolve),
+      children: [
         {
-            path: "/admin-file",
-            component: (resolve) => require(["./views/admin/admin.vue"], resolve),
-            children: [
-                {
-                    path: "",
-                    name: "admin-file",
-                    component: (resolve) => require(["./views/admin/adminFile.vue"], resolve)
-                },
-            ]
-        },
+          path: "",
+          name: "admin-file",
+          component: (resolve) => require(["./views/admin/adminFile.vue"], resolve)
+        }
+      ]
+    },
+    {
+      path: "/admin-page",
+      component: (resolve) => require(["./views/admin/admin.vue"], resolve),
+      children: [
         {
-            path: "/admin-page",
-            component: (resolve) => require(["./views/admin/admin.vue"], resolve),
-            children: [
-                {
-                    path: "",
-                    name: "admin-page",
-                    component: (resolve) => require(["./views/admin/adminPage.vue"], resolve)
-                },
-            ]
-        },
+          path: "",
+          name: "admin-page",
+          component: (resolve) => require(["./views/admin/adminPage.vue"], resolve)
+        }
+      ]
+    },
+    {
+      path: "/admin-timeLine",
+      component: (resolve) => require(["./views/admin/admin.vue"], resolve),
+      children: [
         {
-            path: "/admin-timeLine",
-            component: (resolve) => require(["./views/admin/admin.vue"], resolve),
-            children: [
-                {
-                    path: "",
-                    name: "admin-timeLine",
-                    component: (resolve) => require(["./views/admin/adminTimeLine.vue"], resolve)
-                },
-            ]
-        },
+          path: "",
+          name: "admin-timeLine",
+          component: (resolve) => require(["./views/admin/adminTimeLine.vue"], resolve)
+        }
+      ]
+    },
+    {
+      path: "/admin-log",
+      component: (resolve) => require(["./views/admin/admin.vue"], resolve),
+      children: [
         {
-            path: "/admin-log",
-            component: (resolve) => require(["./views/admin/admin.vue"], resolve),
-            children: [
-                {
-                    path: "",
-                    name: "admin-log",
-                    component: (resolve) => require(["./views/admin/adminLog.vue"], resolve)
-                }
-            ]
-        },
+          path: "",
+          name: "admin-log",
+          component: (resolve) => require(["./views/admin/adminLog.vue"], resolve)
+        }
+      ]
+    },
+    {
+      path: "/admin-setting",
+      component: (resolve) => require(["./views/admin/admin.vue"], resolve),
+      children: [
         {
-            path: "/admin-setting",
-            component: (resolve) => require(["./views/admin/admin.vue"], resolve),
-            children: [
-                {
-                    path: "",
-                    name: "admin-setting",
-                    component: (resolve) => require(["./views/admin/adminSetting.vue"], resolve)
-                }
-            ]
-        },
+          path: "",
+          name: "admin-setting",
+          component: (resolve) => require(["./views/admin/adminSetting.vue"], resolve)
+        }
+      ]
+    },
+    {
+      path: "/admin-user",
+      component: (resolve) => require(["./views/admin/admin.vue"], resolve),
+      children: [
         {
-            path: "/admin-user",
-            component: (resolve) => require(["./views/admin/admin.vue"], resolve),
-            children: [
-                {
-                    path: "",
-                    name: "admin-user",
-                    component: (resolve) => require(["./views/admin/adminUser.vue"], resolve)
-                }
-            ]
-        },
+          path: "",
+          name: "admin-user",
+          component: (resolve) => require(["./views/admin/adminUser.vue"], resolve)
+        }
+      ]
+    },
+    {
+      path: "/admin-role",
+      component: (resolve) => require(["./views/admin/admin.vue"], resolve),
+      children: [
         {
-            path: "/admin-role",
-            component: (resolve) => require(["./views/admin/admin.vue"], resolve),
-            children: [
-                {
-                    path: "",
-                    name: "admin-role",
-                    component: (resolve) => require(["./views/admin/adminRole.vue"], resolve)
-                }
-            ]
-        },
+          path: "",
+          name: "admin-role",
+          component: (resolve) => require(["./views/admin/adminRole.vue"], resolve)
+        }
+      ]
+    },
+    {
+      path: "/admin-res",
+      component: (resolve) => require(["./views/admin/admin.vue"], resolve),
+      children: [
         {
-            path: "/admin-res",
-            component: (resolve) => require(["./views/admin/admin.vue"], resolve),
-            children: [
-                {
-                    path: "",
-                    name: "admin-res",
-                    component: (resolve) => require(["./views/admin/adminRes.vue"], resolve)
-                }
-            ]
-        },
+          path: "",
+          name: "admin-res",
+          component: (resolve) => require(["./views/admin/adminRes.vue"], resolve)
+        }
+      ]
+    },
+    {
+      path: "/admin-userRole",
+      component: (resolve) => require(["./views/admin/admin.vue"], resolve),
+      children: [
         {
-            path: "/admin-userRole",
-            component: (resolve) => require(["./views/admin/admin.vue"], resolve),
-            children: [
-                {
-                    path: "",
-                    name: "admin-userRole",
-                    component: (resolve) => require(["./views/admin/adminUserRole.vue"], resolve)
-                }
-            ]
-        },
+          path: "",
+          name: "admin-userRole",
+          component: (resolve) => require(["./views/admin/adminUserRole.vue"], resolve)
+        }
+      ]
+    },
+    {
+      path: "/admin-roleRes",
+      component: (resolve) => require(["./views/admin/admin.vue"], resolve),
+      children: [
         {
-            path: "/admin-roleRes",
-            component: (resolve) => require(["./views/admin/admin.vue"], resolve),
-            children: [
-                {
-                    path: "",
-                    name: "admin-roleRes",
-                    component: (resolve) => require(["./views/admin/adminRoleRes.vue"], resolve)
-                }
-            ]
-        },
+          path: "",
+          name: "admin-roleRes",
+          component: (resolve) => require(["./views/admin/adminRoleRes.vue"], resolve)
+        }
+      ]
+    },
+    {
+      path: "/admin-articleEditor",
+      component: (resolve) => require(["./views/admin/admin.vue"], resolve),
+      children: [
         {
-            path: "/admin-articleEditor",
-            component: (resolve) => require(["./views/admin/admin.vue"], resolve),
-            children: [
-                {
-                    path: "",
-                    name: "admin-articleEditor",
-                    component: (resolve) => require(["./views/admin/adminArticleEditor.vue"], resolve)
-                }
-            ]
-        },
+          path: "",
+          name: "admin-articleEditor",
+          component: (resolve) => require(["./views/admin/adminArticleEditor.vue"], resolve)
+        }
+      ]
+    }
 
-    ]
+  ]
 })
 
 
-route.beforeEach((to:any,from:any,next:any)=>{
-    if (to.path.startsWith('/admin')) {
-        if(localStorage.getItem('token')){
-            next()
-        }else{
-            next({
-                path:'/articles'
-            })
-        }
+route.beforeEach((to:any, from:any, next:any) => {
+  if (to.path.startsWith("/admin")) {
+    if (localStorage.getItem("token")) {
+      next()
     } else {
-        next()
+      next({
+        path: "/articles"
+      })
     }
-});
+  } else {
+    next()
+  }
+})
 
 export default route
